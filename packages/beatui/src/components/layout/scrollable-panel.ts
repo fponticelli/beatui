5f404c15525dcd4270c0d382f6356d4c289dc805
Franco Ponticelli <franco.ponticelli@gmail.com>
2025-06-24 15:17:12 -0600

    Add ScrollablePanel

---
+++ packages/beatui/src/components/layout/scrollable-panel.ts
@@ -0,0 +1,80 @@
+import {
+  attr,
+  html,
+  TNode,
+  Value,
+  OnDispose,
+  WithElement,
+  prop,
+  on,
+} from '@tempots/dom'
+import { delayed } from '@tempots/std'
+
+export type ScrollablePanelOptions = {
+  header?: TNode
+  footer?: TNode
+  body: TNode
+  shadowOnScroll?: Value<boolean>
+}
+
+export function ScrollablePanel({
+  header,
+  footer,
+  body,
+  shadowOnScroll = true,
+}: ScrollablePanelOptions) {
+  const scrollShadow = prop<'both' | 'top' | 'bottom' | 'none'>('none')
+
+  function updateShadow(target: HTMLElement) {
+    const isAtTop = target.scrollTop === 0
+    const isAtBottom =
+      target.scrollTop + target.clientHeight >= target.scrollHeight - 1
+    if (isAtTop && isAtBottom) {
+      scrollShadow.set('none')
+    } else if (isAtTop) {
+      scrollShadow.set('bottom')
+    } else if (isAtBottom) {
+      scrollShadow.set('top')
+    } else {
+      scrollShadow.set('both')
+    }
+  }
+
+  const panelElement = html.div(
+    attr.class('bc-scrollable-panel'),
+    attr.class(
+      Value.map(shadowOnScroll, (v): string =>
+        v ? 'bc-scrollable-panel--shadow' : ''
+      )
+    ),
+    attr.class(
+      scrollShadow.map((v): string => {
+        switch (v) {
+          case 'both':
+            return 'bc-scrollable-panel--scrolled-up bc-scrollable-panel--scrolled-down'
+          case 'bottom':
+            return 'bc-scrollable-panel--scrolled-up'
+          case 'top':
+            return 'bc-scrollable-panel--scrolled-down'
+          default:
+            return ''
+        }
+      })
+    ),
+    header && html.div(attr.class('bc-scrollable-panel__header'), header),
+    html.div(attr.class('bc-scrollable-panel--header-shadow'), html.div()),
+    html.div(
+      attr.class('bc-scrollable-panel__body'),
+      WithElement(el => OnDispose(delayed(() => updateShadow(el), 10))),
+      on.scroll(event => {
+        const target = event.target as HTMLElement
+        updateShadow(target)
+      }),
+      body
+    ),
+    html.div(attr.class('bc-scrollable-panel--footer-shadow'), html.div()),
+    footer && html.div(attr.class('bc-scrollable-panel__footer'), footer)
+  )
+
+  return panelElement
+}
--- packages/beatui/src/tokens/shadows.ts
+++ packages/beatui/src/tokens/shadows.ts
@@ -12,6 +12,15 @@ export const shadows = {
   lg: '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
   xl: '0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
   '2xl': '0 25px 50px -12px rgb(0 0 0 / 0.25)',
+  'top-2xs': '0 -1px rgb(0 0 0 / 0.05)',
+  'top-xs': '0 -1px 2px 0 rgb(0 0 0 / 0.05)',
+  'top-sm': '0 -1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+  'top-md': '0 -4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+  'top-lg':
+    '0 -10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
+  'top-xl':
+    '0 -20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)',
+  'top-2xl': '0 -25px 50px -12px rgb(0 0 0 / 0.25)',
 } as const
 
 export type ShadowSize = keyof typeof shadows
